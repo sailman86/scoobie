4f3bdba64bf907adafe9b1c5a06285d253441bda
Claudio Famiglietti <claudio.famiglietti@gmail.com>
2022-03-01 18:29:59 +0100

    nedd to fix the destroy action for dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,41 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dogs_params)
+    @dog.user = current_user
+    if @dog.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+    @dog.update(dogs_params)
+    redirect_to user_path(current_user)
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+    @dog.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def dogs_params
+    params.require(:dog).permit(:name, :gender, :age, :breed)
+  end
 end
