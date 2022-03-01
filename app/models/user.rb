c881b64c06efe43ccfba145c68e313c2457c3699
Claudio Famiglietti <claudio.famiglietti@gmail.com>
2022-03-01 14:01:46 +0100

    validation & association & Chatroom channel

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :friendships_as_asker, class_name: "Friendship", foreign_key: :asker_id, dependent: :destroy
+  has_many :friendships_as_receiver, class_name: "Friendship", foreign_key: :receiver_id, dependent: :destroy
+  has_many :dogs, dependent: :destroy
+  has_many :messages
+  has_many :chatrooms, through: :friendships
 end
