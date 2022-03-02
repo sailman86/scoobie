408094dc237677380023afec62d3a9d3a9d4e0d1
Svetoslav Pachev <svetoslavpachevvv@gmail.com>
2022-03-02 17:48:26 +0100

    added bio to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_174839) do
+ActiveRecord::Schema.define(version: 2022_03_02_164731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2022_02_28_174839) do
     t.string "last_name"
     t.string "nickname"
     t.string "address"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
