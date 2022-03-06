7051f08f354eeaba4f2abf239451c5592b4605e2
Kristijan Federer <federer.kristijan@gmail.com>
2022-03-06 15:24:12 +0100

    icons need to be centered

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2022_03_04_104203) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,9 +58,8 @@ ActiveRecord::Schema.define(version: 2022_03_04_104203) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
-    t.float "distance"
     t.string "bio"
+    t.float "distance"
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
