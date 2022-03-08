620303b1d55d05f1a98f414834f0d91d78911bf3
Arne Schmidt <arne.schmidt.eisenach@gmx.de>
2022-03-08 14:46:13 +0100

    works

---
+++ app/javascript/controllers/footer_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+ // static targets = [ "output" ]
+
+  connect() {
+  //  this.outputTarget.textContent = 'Hello, Stimulus!'
+  }
+  upload() {
+    document.querySelector("#custom-button").click()
+  }
+}
