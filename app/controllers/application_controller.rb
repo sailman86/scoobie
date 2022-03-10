5d8690968128251260b39872f0fb0f5bf0a4e37e
Claudio Famiglietti <claudio.famiglietti@gmail.com>
2022-03-10 11:03:14 +0100

    added metatag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def default_url_options
+    { host: ENV["www.scoobie.xyz"] || "localhost:3000" }
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :nickname, :address, :avatar, photos: []])
